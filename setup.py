1d1e47d8c692ffa642f00a6e11f956dab0f6fd93
Ashish Yadav <93119123+ashishyadav58@users.noreply.github.com>
2022-07-31 06:25:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("sudo apt update -y && sudo apt install tor proxychains4 curl -y && rm -rf /etc/proxychains4.conf && cp -r proxychains4.conf /etc")
+os.system("sudo apt update -y && sudo apt install tor proxychains4 git curl -y && rm -rf /etc/proxychains4.conf && cp -r proxychains4.conf /etc")
